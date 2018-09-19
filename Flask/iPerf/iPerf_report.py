fdd8044b4e6565799c50f18dfbb354a8e072a618
hyan1 <hyan1@windriver.com>
2018-09-20 06:29:59 +0800

    create orderdict with generator

--- Flask/iPerf/iPerf_report.py
+++ Flask/iPerf/iPerf_report.py
@@ -4,10 +4,34 @@
 
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
+from collections import OrderedDict
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = "mongodb://localhost:27017/iperf_db"
 mongo = PyMongo(app)
+
+def order_data(data):
+	order_nightly_data = OrderedDict()
+	for item in data:
+		print(item['board'])
+		baseline_data = mongo.db.iperf_up_bl_tb.find_one({"board":item['board']}, {'_id':0})
+		for key in item:
+			order_nightly_data['board'] = item['board']
+			order_nightly_data['Bits'] = item['Bits']
+			order_nightly_data['BSP'] = item['BSP']
+			order_nightly_data['CPU'] = item['CPU']
+			order_nightly_data['Mode'] = item['Mode']
+			order_nightly_data['TCP_64_bl'] = baseline_data['TCP_64']
+			order_nightly_data['TCP_64'] = item['TCP_64']
+			order_nightly_data['TCP_1024_bl'] = baseline_data['TCP_1024']
+			order_nightly_data['TCP_1024'] = item['TCP_1024']
+			order_nightly_data['TCP_65536_bl'] = baseline_data['TCP_65536']
+			order_nightly_data['TCP_65536'] = item['TCP_65536']
+			order_nightly_data['UDP_1400_bl'] = baseline_data['UDP_1400']
+			order_nightly_data['UDP_1400'] = item['UDP_1400']
+			order_nightly_data['spin'] = item['spin']
+			order_nightly_data['run_date'] = item['run_date']
+		yield order_nightly_data
 
 @app.route('/date')
 @app.route('/date/<string:date>')
@@ -18,16 +42,27 @@ def user(date=None):
 	else:
 		#print('=================')
 		#print(date)
-		data = mongo.db.iperf_tb.find({'run_date':date}, {'_id':0})
+		nightly_data = mongo.db.iperf_up_tb.find({'run_date':date}, {'_id':0})
+		order_nightly_data = order_data(nightly_data)
 		#print('=================')
-		#for item in data:
-			#print(type(item))
+		#for item in order_nightly_data:
+			#print(item['board'])
+			
+		#print("++++++++++++++++++")
+		#for item in baseline_data:
+			#print(item)
 			#for key, value in item.items():
 				#print(key, value)
-		if data is not None:
-			return render_template('users.html', datas=data)
+		if order_nightly_data is not None:
+			return render_template('users.html', datas=order_nightly_data)
 		else:
 			return "No data found!"
 
+@app.route('/baseline')
+def get_data():
+	baseline_data = mongo.db.iperf_up_bl_tb.find({}, {'_id':0})
+	return render_template('baseline.html', datas = baseline_data)
+
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
+
